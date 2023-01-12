1b4078d0b9ca4f02cf613792810d29cfc3aaa67f
Rafael Campos Oviedo <104372963+rcampos1970@users.noreply.github.com>
2023-01-12 16:33:49 -0300

    Add files via upload

---
+++ bubble_sorter.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+  int size = 10;
+  int array[size];
+  int t;
+  int i,j;
+  
+  srand(time(NULL));
+  
+  for(i = 0;i<size;i++){
+    array[i] = rand()%100;
+    if(array[i] == array[i-1]){
+      array[i] = rand()%100;
+    }
+  }
+
+  for(i = 0;i<size;i++){
+    printf("%d\t",array[i]);
+  }
+  
+
+  for(i=0;i<size;i++){
+    for(j=0;j<size-1;j++){
+      if(array[j+1]<array[j]){
+	t = array[j];
+	array[j] = array[j+1];
+	array[j+1] = t;
+      }
+    }
+  }
+  printf("\n");
+  for(i = 0;i<size;i++){
+    printf("%d\t",array[i]);
+  }
+  printf("\n");
+  return 0;
+}
