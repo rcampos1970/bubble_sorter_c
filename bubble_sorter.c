ba93b1494eceee69266cf4d9ab3a9c37bfbc6f5f
Rafael Campos Oviedo <104372963+rcampos1970@users.noreply.github.com>
2023-10-10 15:39:58 -0300

    Update bubble_sorter.c

--- bubble_sorter.c
+++ bubble_sorter.c
@@ -1,49 +1,44 @@
-#include<stdio.h>
-// standard library contains functions for rand and srand
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void){
+int main(void) {
   int size = 10;
   int array[size];
-  // integer variable t is temporary, used later to do the changing part
   int t;
-  int i,j;
-  
- // srand is the seed (in this case the time is beeing used)
-// this makes a true random number every time
-  srand(time(NULL));
-  
-// creating the random array with a defined size
-  for(i = 0;i<size;i++){
-    array[i] = rand()%100;
-  // just making sure that no adjacent number are equal  
-  if(array[i] == array[i-1]){
-      array[i] = rand()%100;
-    }
-  }
+  int i, j;
 
-// printing array
-  for(i = 0;i<size;i++){
-    printf("%d\t",array[i]);
-  }
-  
- // BUBLLE SORTER
-  for(i=0;i<size;i++){
-    for(j=0;j<size-1;j++){
-      if(array[j+1]<array[j]){
-	// changing part
-        t = array[j];
-	array[j] = array[j+1];
-	array[j+1] = t;
+  srand(time(NULL));
+
+  for (i = 0; i < size; i++) {
+    array[i] = rand() % 100;
+    if (i > 0) {
+      while (array[i] == array[i - 1]) {
+        array[i] = rand() % 100;
       }
     }
   }
-  printf("\n");
-  // printing array again
-   for(i = 0;i<size;i++){
-    printf("%d\t",array[i]);
+
+  printf("Original array:\n");
+  for (i = 0; i < size; i++) {
+    printf("%d\t", array[i]);
+  }
+
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size - 1; j++) {
+      if (array[j + 1] < array[j]) {
+        t = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = t;
+      }
+    }
+  }
+
+  printf("\nSorted array:\n");
+  for (i = 0; i < size; i++) {
+    printf("%d\t", array[i]);
   }
   printf("\n");
+
   return 0;
 }
